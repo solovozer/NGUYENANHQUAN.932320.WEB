d20289b0f6a61cf47aa6c28793ae76315d348dea
Integralsia <akvegetacocatrada3@gmail.com>
2025-10-18 11:09:40 +0700

    added la6

---
+++ lab6/code/script.js
@@ -0,0 +1,26 @@
+var dogs = document.getElementById("d-c").style;
+var cats = document.getElementById('c-c').style;
+var cp = document.getElementById("c-c").getElementsByTagName("img")[0].style;
+var dp = document.getElementById('d-c').getElementsByTagName("img")[0].style;
+
+function doShit(kw) {
+    if (kw === "cats") {
+        cats.width = "96%";
+        dogs.width = "10%";
+        cp.width = "80%";
+        dp.width = "0%";  
+        cats.justifyContent = "left";
+    } else if (kw === "dogs") {
+        cats.width = "10%";
+        dogs.width = "96%";
+        cp.width = "0%";
+        dp.width = "80%";
+        dogs.justifyContent = "left";
+    } else if (kw === "both") {
+        cats.width = "50%";
+        dogs.width = "50%";
+        cp.width = dp.width = "300px";
+        dogs.justifyContent = "center";
+        cats.justifyContent = "center";
+    }
+}
