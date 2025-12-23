ff47ffd1742b179709d1823b9d27cd5a1a00ff64
Integralsia <akvegetacocatrada3@gmail.com>
2025-12-23 11:22:59 +0700

    commit suicide

--- lab14/app.py
+++ lab14/app.py
@@ -1,41 +1,17 @@
-"""from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    user_data = {"name": "Alex", "role": "Admin"}
-    items = ["Dashboard", "Settings", "Log Out"]
+@app.route('/about', methods=['GET', 'POST'])
+def calculate():
+    result = None
+    if request.method == 'POST':
+        num1 = request.form.get('num1', type=int)
+        num2 = request.form.get('num2', type=int)
+        result = num1 + num2
     
-    return render_template('index.html', user=user_data, menu=items)
-
-
-if __name__ == '__main__':
-    app.run(debug=True)"""
-
-
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from flask_admin import Admin
-from flask_admin.contrib.sqla import ModelView
-
-app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-
-db = SQLAlchemy(app)
-
-
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120))
-
-app.config['FLASK_ADMIN_SWATCH'] = 'cerulean' 
-admin = Admin(app, name='My Dashboard')
-admin.add_view(ModelView(User, db.session))
+    # Always render the same template
+    return render_template('about.html', result=result)
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  
     app.run(debug=True)
